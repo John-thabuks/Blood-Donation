f2e05d6f9b6771ab9c13c2b1e8e886a101a82f29
John-thabuks <thabuksjohn@gmail.com>
2024-03-28 16:32:21 +0300

    folder set-ups

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import React from "react"
+import About from "./pages/About"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Blood Donation System</h1>
-    </>
-  )
+const App = () => {
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
